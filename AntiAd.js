dde233be62ee85c866f102f7a3720fc20134af88
danlitvak <daniel.litvak.64@gmail.com>
2025-01-06 03:07:20 +0000

    improved antiad

--- AntiAd.js
+++ AntiAd.js
@@ -1,2 +1,7 @@
 // automatically close the p5js ads (hopefully)
+
+console.log("Running Anti Ad Script ...");
+
+document.getElementsByClassName("Banner_close__8VnLH")[0].addEventListener('click', () => {console.log("I have been defeated")});
+
 document.getElementsByClassName("Banner_close__8VnLH")[0].click();
