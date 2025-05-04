f8fccbb72a1548e9a813b74ca1d00154950e6efb
Pro-grammer3764 <daniellitvak3764@gmail.com>
2025-05-04 11:34:29 -1000

    added p5 project template to make future projects easier and added project 8 (hamiltonian path solver)

---
+++ projects/project-8/project-8-sketch.js
@@ -0,0 +1,34 @@
+// integration starts here
+// variables to keep track
+let canvasHeight = 400;
+let canvasWidth = 400;
+let darkMode = document.body.classList.contains("dark-mode");
+let currentBackground = 0;
+
+// function for resizing
+function windowResized() {
+    container = document.getElementById('canvas-container');
+    canvasWidth = container.getBoundingClientRect().width;
+    canvasHeight = canvasWidth; // square canvas
+    resizeCanvas(canvasWidth, canvasHeight);
+}
+
+function setup() {
+    // start up
+    container = document.getElementById('canvas-container');
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent(container);
+    windowResized();
+    // integration ends here
+}
+
+function draw() {
+    // Handle dark mode background transition
+    darkMode = document.body.classList.contains("dark-mode");
+    if (darkMode) {
+        currentBackground = lerp(currentBackground, 0, 0.1);
+    } else {
+        currentBackground = lerp(currentBackground, 200, 0.1);
+    }
+    background(currentBackground);
+}
